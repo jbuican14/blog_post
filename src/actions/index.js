db7dd4970e6011dc5b5f87f09fbc237ba772fe3b
Juti Buican <jbuican19@gmail.com>
2021-04-03 16:56:39 +0100

    Created action using thunk (before refactoring action)

---
+++ src/actions/index.js
@@ -0,0 +1,13 @@
+import { FETCH_POSTS } from '../constants';
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+export const fetchPosts = () => {
+  return async function (dispatch, getState) {
+    const res = await jsonPlaceholder.get('/posts');
+
+    dispatch({
+      type: FETCH_POSTS,
+      payload: res,
+    });
+  };
+};
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchPosts } from '../actions';
 
-class PostList extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Post List</h1>
-      </div>
-    );
-  }
-}
+const PostList = () => {
+  const dispatch = useDispatch();
+
+  useState(() => {
+    dispatch(fetchPosts());
+  });
+
+  return (
+    <div>
+      <h1> Post List</h1>
+    </div>
+  );
+};
 
 export default PostList;
