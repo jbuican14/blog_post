9d397edd8576a9e181137c7caa799fdfbddebe1c
Juti Buican <jbuican19@gmail.com>
2021-04-04 20:40:36 +0100

    Rendering posts in PostList component

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -8,12 +8,24 @@ const PostList = () => {
 
   useState(() => {
     dispatch(fetchPosts());
-  });
+  }, [dispatch]);
 
   return (
-    <div>
+    <div className="ui relaxed divided list">
       <h1> Post List</h1>
       {console.log(posts)}
+      {posts.length > 0 &&
+        posts.map((p) => (
+          <div className="item" key={p.id}>
+            <i className="large middle aligned icon user" />
+            <div className="content">
+              <div className="description">
+                <h2>{p.title}</h2>
+                <p>{p.body}</p>
+              </div>
+            </div>
+          </div>
+        ))}
     </div>
   );
 };
